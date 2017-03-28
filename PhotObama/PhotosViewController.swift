a1e86d4f625b9e52da7b49ebaa0f894622b423bb
lindsaypond <lindsay.pond@nbcuni.com>
2017-03-28 15:49:32 -0700

    Set up main view controller and PhotosViewController

---
+++ PhotObama/PhotosViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotosViewController.swift
+//  PhotObama
+//
+//  Created by Lindsay Pond on 3/28/17.
+//  Copyright © 2017 Lindsay Pond. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
